3f9ba626e1e60bffdac2a40eef4e201d5d5bca16
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-08-25 17:09:33 +0300

    feat(main): init split commits

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::process::Command;
+
+#[derive(Debug)]
+pub struct Commit {
+    hash: String,
+    name: String,
+}
+
+impl Commit {
+    pub fn new(hash: String, name: String) -> Self {
+        Self { hash, name }
+    }
+}
+
+fn main() {
+    let cmd = String::from_utf8(
+        Command::new("git")
+            .arg("log")
+            .arg("--pretty=format:\"%h,%s\"")
+            .output()
+            .expect("failed to output")
+            .stdout,
+    )
+    .expect("failed to parse utf8");
+
+    let commits = cmd
+        .lines()
+        .map(|v| {
+            let lol = v.split(',').map(|v| v.to_owned()).collect::<Vec<String>>();
+
+            debug_assert!(lol.len() == 2);
+
+            Commit::new(lol[0].clone(), lol[1].clone())
+        })
+        .collect::<Vec<Commit>>();
+
+    println!("{commits:?}");
+
+    for commit in commits {}
+}
