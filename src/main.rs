60af9c7190a47754f51a1cdc0809f111214bcc09
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-08-25 19:48:26 +0300

    feat(main): state build command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use clap_derive::Parser;
-use std::process::Command;
+use std::{borrow::Cow, marker::PhantomData, mem, process::Command};
 
 #[derive(Debug)]
 pub struct Commit {
@@ -20,18 +20,77 @@ struct Cli {
     ancestry_path: Option<String>,
 }
 
+trait State {}
+
+struct Init;
+struct Run;
+
+impl State for Init {}
+impl State for Run {}
+
+struct CustomCommand<T: State> {
+    command: &'static str,
+    args: Vec<String>,
+    _state: PhantomData<T>,
+}
+
+impl<T: State> CustomCommand<T> {
+    fn transition<S: State>(self) -> CustomCommand<S> {
+        CustomCommand {
+            command: self.command,
+            args: self.args,
+            _state: PhantomData,
+        }
+    }
+}
+
+impl CustomCommand<Init> {
+    fn new(command: &'static str) -> CustomCommand<Init> {
+        CustomCommand {
+            command,
+            args: Vec::new(),
+            _state: PhantomData,
+        }
+    }
+
+    fn add_args(mut self, args: impl IntoIterator<Item = Cow<'static, str>>) -> Self {
+        self.args.extend(args.into_iter().map(|v| v.to_string()));
+
+        self
+    }
+
+    fn build(self) -> CustomCommand<Run> {
+        self.transition()
+    }
+}
+
+impl CustomCommand<Run> {
+    fn run(&mut self) -> String {
+        String::from_utf8(
+            Command::new(self.command)
+                .args(mem::take(&mut self.args))
+                .output()
+                .expect("failed to output")
+                .stdout,
+        )
+        .expect("failed to parse utf8")
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    let cmd = String::from_utf8(
-        Command::new("git")
-            .arg("log")
-            .arg("--pretty=format:\"%h,%s\"")
-            .output()
-            .expect("failed to output")
-            .stdout,
-    )
-    .expect("failed to parse utf8");
+    let mut args = vec!["log", "--pretty=format:\"%h,%s\""]
+        .into_iter()
+        .map(Into::into)
+        .collect::<Vec<Cow<'static, str>>>();
+
+    if let Some(path) = cli.ancestry_path {
+        println!("{path}");
+        args.push(path.into());
+    }
+
+    let cmd = CustomCommand::new("git").add_args(args).build().run();
 
     let commits = cmd
         .lines()
